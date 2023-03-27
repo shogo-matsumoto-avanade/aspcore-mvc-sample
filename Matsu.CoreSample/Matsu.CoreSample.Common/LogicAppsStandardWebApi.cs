763320f0d5e863733b1b112f4a2b8893245e4afc
Shogo Matsumoto <shogo.matsumoto@avanade.com>
2023-03-27 13:20:05 +0900

    create new project

---
+++ Matsu.CoreSample/Matsu.CoreSample.Common/LogicAppsStandardWebApi.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Matsu.CoreSample.Common
+{
+    public class LogicAppsStandardWebApi
+    {
+        public async Task<string> CallHttpTrigger(string url, string signature)
+        {
+            var client = new HttpClient();
+
+            var request = $"{url}&sig={signature}";
+            var response = await client.GetAsync(request);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
---
+++ Matsu.CoreSample/Matsu.CoreSample.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Matsu.CoreSample.Common;
+using Matsu.CoreSample.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Matsu.CoreSample.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetWorkFlowResults()
+        {
+            var api = new LogicAppsStandardWebApi();
+            return api.CallHttpTrigger("https://la4standard.azurewebsites.net:443/api/WF-HTTPtrigger/triggers/manual/invoke?api-version=2022-05-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0", "tFJChuAq-AwUYqVJIOrtFrsGlQcqG7QA3JiXE6YhB0c").Result.ToString();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
